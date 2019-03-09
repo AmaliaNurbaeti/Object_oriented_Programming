1be62cdee44f110ec06b016ac219e9bfff9cb683
AmaliaNurbaeti <48266220+AmaliaNurbaeti@users.noreply.github.com>
2019-03-09 15:25:23 +0700

    Add files via upload

---
+++ Amalia_Nurbaeti_17090140_4C.java
@@ -0,0 +1,17 @@
+
+package output;
+
+/**
+ *
+ * @author amal
+ */
+public class Amalia_Nurbaeti_17090140_4C {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+               System.out.println(" Welcome To Object Oriented Programing \n Amalia_Nurbaeti_17090140_4C"); 
+    }
+    
+}
